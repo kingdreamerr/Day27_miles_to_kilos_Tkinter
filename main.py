8905b45968715edb731beaa32078dd9c00954b17
kingdreamerr <jackmvula458@gmail.com>
2023-03-01 14:01:59 +0200

    Add lables and fix them on the screen using grid

--- main.py
+++ main.py
@@ -8,4 +8,9 @@ input_tag.grid(column=1, row=0)
 miles = Label(text="Miles",font=('Arial',15,'bold'))
 miles.grid(column=2, row=0)
 
+equal = Label(text='Is equal to',font=('Arial',15,'bold'))
+equal.grid(column=0, row=1)
+
+value = Label(text='0',font=('Arial',15,'bold'))
+value.grid(column=1, row=1)
 window.mainloop()
