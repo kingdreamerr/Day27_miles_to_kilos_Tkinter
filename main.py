ab48e28cb8a959b47890f24fd8d55158ab167589
kingdreamerr <jackmvula458@gmail.com>
2023-03-01 14:01:10 +0200

    Add entry field and a lable and set their positions on the screen

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from tkinter import *
 
 window = Tk()
 
+input_tag = Entry(width=10)
+input_tag.grid(column=1, row=0)
 
+miles = Label(text="Miles",font=('Arial',15,'bold'))
+miles.grid(column=2, row=0)
 
 window.mainloop()
